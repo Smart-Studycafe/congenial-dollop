e284c9063511a84c39d43245dbe338a38a7f2871
최율(***9***069) <choiyool00@kau.kr>
2023-11-20 17:38:50 +0900

    (feat) Implemented validator that validates userid and username
    Implemented with Regex.

---
+++ server/utils/validator.py
@@ -0,0 +1,16 @@
+import re
+
+class Validator:
+    def validate_user_id(input_id: str):
+        pattern = r'^[0-9]+$'
+        if re.match(pattern, input_id):
+            return True
+        else:
+            return False
+        
+    def validate_username(input_name: str):
+        pattern = r'^[가-힣a-zA-Z0-9]{1,10}$'
+        if re.match(pattern, input_name):
+            return True  # 일치하는 경우
+        else:
+            return False  # 불일치하는 경우
