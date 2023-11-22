7583aedb76e9e953e09543ea8bc7b3eb38984bce
최율(***9***069) <choiyool00@kau.kr>
2023-11-23 00:09:52 +0900

    (feat) Implemented DB Connector and DB Query Executor
    For clean code, implemented DB connector class and query executor.
    In every transaction method, it returns result of query execution.
    Also, query executor has parameter: DB connector for encapsulation

--- server/utils/database_utils.py
+++ server/utils/database_utils.py
@@ -1,15 +1,62 @@
 from dotenv import load_dotenv
+from .message_maker import Message
+from . import serializer
 import pymysql
 import os
+import sys
 load_dotenv()
 
-def connect_db():
-    connection = pymysql.connect(
-        host=os.getenv("DB_HOST"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USERNAME"),
-        password=os.getenv("DB_PASSWORD"),
-        ssl_ca=os.getenv("SSL_CERT")
-    )
-
-    return connection
+class DatabaseConnector:
+    def __init__(self):
+        pass
+    
+    def connect(self):
+        try:
+            self.connection = pymysql.connect(
+                host=os.getenv("DB_HOST"),
+                database=os.getenv("DB_NAME"),
+                user=os.getenv("DB_USERNAME"),
+                password=os.getenv("DB_PASSWORD"),
+                ssl_ca=os.getenv("SSL_CERT")
+            )
+            return self.connection
+        except:
+            return False
+    
+    def disconnect(self):
+        self.connection.close()
+    
+class DatabaseQueryExecutor:
+    def select_seats(conn):
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        
+        with conn:
+            sql = "select * from seats"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            result = serializer.make_serializable_datetimes(result)
+        
+        return result
+    
+    def insert_user(conn, user_id, username):
+        cursor = conn.cursor()
+        
+        with conn.cursor() as cursor:
+            sql = f"insert into user values({user_id}, \"{username}\")"
+            result = cursor.execute(sql)
+            if result:
+                conn.commit()
+                result = {"result": "Transaction success."}
+                return Message.Success("Transaction success.")
+            else:
+                return Message.Success(f"Transaction failed while inserting values {user_id}, \"{username}\"")
+    
+    def select_users(conn):
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        
+        with conn:
+            sql = "select * from user"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+        
+        return result
