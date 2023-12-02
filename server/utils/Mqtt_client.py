e0b414b4bdd426cbe84c7ce4a63e34542506ec6f
최율(***9***069) <choiyool00@kau.kr>
2023-12-02 18:44:19 +0900

    (feat) Implemented mqtt publisher

---
+++ server/utils/Mqtt_client.py
@@ -0,0 +1,30 @@
+import paho.mqtt.client as mqtt
+from dotenv import load_dotenv
+import os, time, sys
+load_dotenv()
+
+class MqttClient():
+    def __init__(self):
+        self.host = os.getenv("CORE_ENDPOINT")
+        self.ca = os.getenv("CORE_CA")
+        self.cert = os.getenv("CORE_CERT")
+        self.key = os.getenv("CORE_KEY")
+        self.client = mqtt.Client()
+        self.client.tls_set(self.ca, self.cert, self.key)
+    
+    def publish(self, device_id, buzzflag):
+        topic = "smartstudy/buzzer"
+        self.client.connect(host=self.host, port=8883)
+        self.client.loop_start()
+        while True:
+            time.sleep(1)
+            message = f'{{"device_id": {device_id}, "buzzflag": {buzzflag}}}'
+            result = self.client.publish(topic, message)
+            status = result[0]
+            if status == 0:
+                print(f"Send `{message}` to topic `{topic}`")
+                break
+            else:
+                print(f"Failed to send message to topic {topic}")
+        self.client.loop_stop()
+        self.client.disconnect()
