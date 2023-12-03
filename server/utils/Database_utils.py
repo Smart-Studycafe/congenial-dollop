f4ac4f4b4bb06c9a7682707cdd5af5c9ee9b982e
highlaw00 <65754646+highlaw00@users.noreply.github.com>
2023-12-04 00:55:54 +0900

    Revert "Revert "Back-end #1. Skeleton initialized""

---
+++ server/utils/Database_utils.py
@@ -0,0 +1,126 @@
+import sys
+from dotenv import load_dotenv
+from .Message_maker import Message
+from .Serializer import Serializer
+import pymysql, os, json
+load_dotenv()
+
+class DatabaseConnector:
+    def __init__(self):
+        self.connection = None
+    
+    def connect(self):
+        try:
+            self.connection = pymysql.connect(
+                host=os.getenv("DB_HOST"),
+                database=os.getenv("DB_NAME"),
+                user=os.getenv("DB_USERNAME"),
+                password=os.getenv("DB_PASSWORD"),
+                ssl_ca=os.getenv("SSL_CERT")
+            )
+            return self.connection
+        except:
+            return False
+    
+    def disconnect(self):
+        self.connection.close()
+    
+class DatabaseQueryExecutor:
+    def select_keys(conn):
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        
+        with conn:
+            sql = "select value from api_key"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+        
+        return result
+    
+    def select_one_seat(conn,seat_id):
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+
+        with conn:
+            sql = f"select * from seats where seat_id={seat_id}"
+            cursor.execute(sql)
+            result = cursor.fetchone()
+            result = Serializer.make_serializable_one_datetime(result)
+        
+        return result
+
+    def select_seats(conn):
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        
+        with conn:
+            sql = "select * from seats"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            result = Serializer.make_serializable_datetimes(result)
+        
+        return result
+    
+    def insert_seats(conn, seat_id, user_id, usage_start, usage_end):
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        usage_start = f'"{usage_start}"'
+        usage_end = f'"{usage_end}"'
+        values = [seat_id, user_id, usage_start, usage_end]
+        values = ', '.join(values)
+        
+        with conn:
+            # foreign key constraints check.
+            sql = f"select * from users"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            is_obeying = False
+            for record in result:
+                if "user_id" in record and record["user_id"] == int(user_id):
+                    is_obeying = True
+                    break
+            
+            if not is_obeying:
+                raise ValueError("Foreign Key constraint violated.")
+            
+            sql = f"insert into seats values({values})"
+            result = cursor.execute(sql)
+            if result:
+                conn.commit()
+                return True
+            else:
+                return False
+    
+    def modify_seats(conn, seat_id, user_id, usage_start, usage_end):
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        usage_start = f'"{usage_start}"'
+        usage_end = f'"{usage_end}"'
+        values = [seat_id, user_id, usage_start, usage_end]
+        values = ', '.join(values)
+        
+        with conn:
+            modify_sql = f"update seats\
+                set seat_id={seat_id}, user_id={user_id}, usage_start={usage_start}, usage_end={usage_end}\
+                where seat_id={seat_id}"
+            cursor.execute(modify_sql)
+            conn.commit()
+
+        return True
+        
+    def insert_user(conn, user_id, username):
+        cursor = conn.cursor()
+        
+        with conn.cursor() as cursor:
+            sql = f"insert into users values({user_id}, \"{username}\")"
+            result = cursor.execute(sql)
+            if result:
+                conn.commit()
+                return Message.Success("Transaction success.")
+            else:
+                return Message.Success(f"Transaction failed while inserting values {user_id}, \"{username}\"")
+    
+    def select_users(conn):
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        
+        with conn:
+            sql = "select * from users"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+        
+        return result
---
+++ server/utils/Serializer.py
@@ -0,0 +1,28 @@
+import datetime
+
+class Serializer:
+
+    def make_serializable_one_datetime(obj):
+        start_dt = obj['usage_start']
+        end_dt = obj['usage_end']
+        
+        start_str = start_dt.strftime("%Y-%m-%d %H:%M:%S")
+        end_str = end_dt.strftime("%Y-%m-%d %H:%M:%S")
+        
+        obj['usage_start'] = start_str
+        obj['usage_end'] = end_str
+    
+        return obj
+
+    def make_serializable_datetimes(obj_arr):
+        for idx, obj in enumerate(obj_arr):
+            start_dt = obj['usage_start']
+            end_dt = obj['usage_end']
+            
+            start_str = start_dt.strftime("%Y-%m-%d %H:%M:%S")
+            end_str = end_dt.strftime("%Y-%m-%d %H:%M:%S")
+            
+            obj_arr[idx]['usage_start'] = start_str
+            obj_arr[idx]['usage_end'] = end_str
+        
+        return obj_arr
